e1a576fa24ef8880d6c49b20439c6e18d43b9124
highgw <hie2gw@gmail.com>
2025-12-18 17:27:42 +0900

    [feat] add docs

--- lib/nehing_generator.dart
+++ lib/nehing_generator.dart
@@ -1,3 +1,8 @@
+/// A Dart library for generating meaningless but funny random Korean text
+/// (commonly known as 'Nehing').
+///
+/// This package is intended for dummy data, testing, games, and fun.
+/// It does not generate meaningful Korean sentences.
 library nehing_generator;
 
 import 'dart:math';
@@ -17,6 +22,19 @@ class Nehing {
     '', 'ㄱ','ㄴ','ㄷ','ㄹ','ㅁ','ㅂ','ㅅ','ㅇ','ㅈ','ㅊ','ㅋ','ㅌ','ㅍ','ㅎ'
   ];
 
+  /// Generates a random Korean text string.
+  ///
+  /// The returned string consists of randomly combined Hangul syllables.
+  ///
+  /// - [length] specifies how many syllables will be generated.
+  /// - If [finalConsonant] is set to false, generated syllables
+  ///   will not include final consonants (받침).
+  ///
+  /// Example:
+  /// ```dart
+  /// Nehing.generate(length: 4);
+  /// Nehing.generate(length: 3, finalConsonant: false);
+  /// ```
   static String generate({
     int length = 2,
     bool finalConsonant = true,
@@ -27,6 +45,11 @@ class Nehing {
     ).join();
   }
 
+  /// Generates a single random Hangul syllable.
+  ///
+  /// This method randomly selects initial, medial, and optional
+  /// final consonants and combines them into a valid Hangul
+  /// Unicode syllable.
   static String _randomSyllable(bool finalConsonant) {
     final cho = _chosung[_random.nextInt(_chosung.length)];
     final jung = _jungsung[_random.nextInt(_jungsung.length)];
